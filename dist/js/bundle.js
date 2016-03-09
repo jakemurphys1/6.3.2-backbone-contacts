58c476e3cd15f2ce6960c53de6e3018177f95b18
jakemurphys1 <jakemurphys1@gmail.com>
2016-03-09 16:32:13 -0500

    second

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -6,11 +6,20 @@ var $ = require("jquery");
 var Collection = require("./models/contacts")
 var Contacts = require("./views/contacts")
  var myCollection = new Collection.contactCollection();
+ var myModel=new Collection.Contact();
+ console.log(myModel)
+
+
 
  $("#addForm").submit(function(event){
-  event.preventDefault();
+     event.preventDefault();
+   if($("#fname").val()===""){
+     console.log("nothing there")
+     return
+   }
+
  $("#fname").val="";
-  var contactList = new Character({name:$("#fname").val(), Email: $("#femail").val(), Phone:$("#fphone").val(), Twitter: $("#ftwitter").val(), Linkedin:$("#flink").val()})
+  var contactList = new myModel({name:$("#fname").val(), Email: $("#femail").val(), Phone:$("#fphone").val(), Twitter: $("#ftwitter").val(), Linkedin:$("#flink").val()})
   contactList.save();
 })
 
